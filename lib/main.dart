e8fddd26f37f531616c263c265706ac24bf2b4b2
Oniya Daniel <daniel.prince84@gmail.com>
2019-11-18 05:18:39 +0100

    added music by genre

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -43,8 +45,8 @@ class Player extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           mainAxisSize: MainAxisSize.max,
           children: <Widget>[
-
             RecommendedPlaylist(),
+            MusicByGenre(),
           ],
         ),
 
@@ -122,6 +124,7 @@ class Player extends StatelessWidget {
                   TabButton(
                     titleString: "Home",
                     iconData: Icons.home,
+                    isActive: true,
                   ),
                   TabButton(
                     titleString: "Flow",
@@ -150,8 +153,9 @@ class Player extends StatelessWidget {
 class TabButton extends StatelessWidget {
   final String titleString;
   final IconData iconData;
+  final bool isActive;
 
-  TabButton({this.titleString, this.iconData});
+  TabButton({this.titleString, this.iconData, this.isActive = false});
 
   @override
   Widget build(BuildContext context) {
@@ -161,8 +165,16 @@ class TabButton extends StatelessWidget {
         padding: EdgeInsets.all(8.0),
         child: Column(
           children: <Widget>[
-            Icon(iconData),
-            Text(titleString),
+            Icon(
+              iconData,
+              color: isActive ? Colors.black : Colors.black26,
+            ),
+            Text(
+              titleString,
+              style: TextStyle(
+                color: isActive ? Colors.black : Colors.black38,
+              ),
+            ),
           ],
         ),
         onPressed: () => {},
@@ -291,9 +303,11 @@ class RecommendedPlaylist extends StatelessWidget {
 
                                     /** TITLE DISPLAY */
                                     Column(
-                                      crossAxisAlignment: CrossAxisAlignment.center,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.center,
                                       mainAxisSize: MainAxisSize.max,
-                                      mainAxisAlignment: MainAxisAlignment.center,
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
                                       children: <Widget>[
                                         Text(
                                           item["title"].toLowerCase(),
@@ -355,6 +369,121 @@ class RecommendedPlaylist extends StatelessWidget {
                     ));
 
                 return poles;
+              })(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class MusicByGenre extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Padding(
+      padding: const EdgeInsets.only(top: 30.0, bottom: 20.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          GestureDetector(
+            child: Padding(
+                padding: const EdgeInsets.only(
+                  left: 20,
+                  right: 20,
+                ),
+                child: Row(
+                  children: <Widget>[
+                    Text(
+                      "Music by genre",
+                      style: TextStyle(
+                          fontSize: 20.0, fontWeight: FontWeight.w700),
+                    ),
+                    Icon(Icons.chevron_right),
+                  ],
+                )),
+            onTap: () => {},
+          ),
+          SingleChildScrollView(
+            padding: const EdgeInsets.only(
+              left: 20,
+              right: 20,
+            ),
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+
+              children: (() {
+                List<Widget> genreWidgets = [];
+
+                List<String> genres = [
+                  "Pop", "Dance & EDM", "R & B", "Electronic", "Rock",
+                  "Blues", "Soul & Funk", "Rap", "African music", "Raggae",
+                  "Soundtracks", "Jazz",
+                ];
+
+                genres.forEach((item) => genreWidgets.add(
+                    Padding(
+                      padding: const EdgeInsets.only(top: 16.0, right: 16.0),
+                      child: Container(
+                        width: 148,
+                        height: 68,
+                        decoration: BoxDecoration(
+                          color: Color.fromRGBO( Random().nextInt(155),
+                              Random().nextInt(155), Random().nextInt(155), 1),
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                        child: Center(
+                          child: Text(
+                            item,
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                ));
+
+                genreWidgets.add(
+                  Padding(
+                    padding: const EdgeInsets.only(top: 16.0, right: 16.0),
+                    child: Container(
+                      width: 148,
+                      height: 68,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 16.0, right: 16.0,),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          mainAxisSize: MainAxisSize.max,
+                          children: <Widget>[
+                            Text(
+                              "View all",
+                              textAlign: TextAlign.start,
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 16,
+                              ),
+                            ),
+                            Spacer(),
+                            Icon(
+                              Icons.chevron_right
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+
+                return genreWidgets;
               })(),
             ),
           ),
